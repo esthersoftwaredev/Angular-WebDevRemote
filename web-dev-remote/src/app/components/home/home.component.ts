1807d01d533a702a4d7fb12e059f026cf67da412
Esther White <monacodelisa@gmail.com>
2023-12-26 00:15:14 +0200

    ok for now

--- web-dev-remote/src/app/components/home/home.component.ts
+++ web-dev-remote/src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink} from "@angular/router";
+
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
