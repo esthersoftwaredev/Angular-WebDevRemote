8ec62d9db150f8b9f0e6e2b079e73a0f19178328
Esther White <monacodelisa@gmail.com>
2023-12-26 09:31:15 +0200

    footer component to add custom svg mat icon

--- web-dev-remote/src/app/app.component.ts
+++ web-dev-remote/src/app/app.component.ts
@@ -2,26 +2,28 @@ import { MediaMatcher } from "@angular/cdk/layout";
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./components/footer/footer.component";
 
-import { MatListModule } from "@angular/material/list";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
 	imports: [
 		CommonModule,
+    FooterComponent,
     RouterLink,
 		RouterLinkActive,
 		RouterOutlet,
 		MatToolbarModule,
-		MatButtonModule,
-		MatIconModule,
 		MatSidenavModule,
 		MatListModule,
+		MatButtonModule,
+		MatIconModule,
 	],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
